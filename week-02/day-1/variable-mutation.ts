ad267f5dd062e7ce1fb4fd26243dbd19699e4521
Ágnes Molnár <agnes.molnar@gmx.com>
2018-10-29 12:43:07 +0100

    created variable-mutation.ts

---
+++ week-02/day-1/variable-mutation.ts
@@ -0,0 +1,44 @@
+'use strict';
+
+let a: number=3;
+a=3*10;
+console.log(a);
+
+let b: number=100;
+b= b-7;
+console.log(b);
+
+let c: number=44;
+c=c*2;
+console.log(c);
+
+let d: number = 125;
+d=d/5;
+console.log(d);
+
+let e: number = 8;
+e=e*e*e;
+console.log(e)
+
+let f1: number = 123;
+let f2: number = 345;
+console.log(f1 > f2);
+
+let g1: number = 350;
+let g2: number = 200;
+
+console.log((g2*2)>g1);
+
+let h: number = 1357988018575474;
+
+let i1: number = 10;
+let i2: number = 3;
+
+console.log((i2*i2) < i1) ;
+
+let j: number = 1521;
+console.log(j%3===0 );
+
+let k: string = 'Apple';
+k= k + k+ k+ k;
+console.log(k);
