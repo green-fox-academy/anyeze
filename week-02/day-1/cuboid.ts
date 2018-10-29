2c2d5e5179adebd502fef2d8e8a51a0ea8cfb7ea
Ágnes Molnár <agnes.molnar@gmx.com>
2018-10-29 14:06:42 +0100

    created cuboid.ts

---
+++ week-02/day-1/cuboid.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+let a: number= 8;
+let b: number=5;
+let c: number=6;
+
+let surFaceArea: number= 2*(a*b+b*c+b*c);
+let volume: number=a*b*c;
+console.log("Surface Area: " + surFaceArea);
+console.log("Volume: " + volume);
+
+
