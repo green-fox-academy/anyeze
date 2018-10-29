0ba8cca3c73cd43919b3a2e5c11c55d020b58553
Ágnes Molnár <agnes.molnar@gmx.com>
2018-10-29 14:14:01 +0100

    created seconds-in-a-day.ts

---
+++ week-02/day-1/seconds-in-a-day.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+let currentHours: number = 14;
+let currentMinutes: number = 34;
+let currentSeconds: number = 42;
+
+let remainingSeconds: number= 60-currentSeconds;
+
+console.log("Remaining seconds: "+ remainingSeconds);
