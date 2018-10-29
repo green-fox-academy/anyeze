ac3134dba388f6443d011faec66ba32aabdd18a7
Ágnes Molnár <agnes.molnar@gmx.com>
2018-10-29 16:50:34 +0100

    created draw-triangle.ts

---
+++ week-02/day-1/draw-triangle.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+let lineCount: number = 4;
+let output1: string='*';
+let output2: string='*';
+
+for(let i: number=0; i < lineCount; i++){
+    console.log(output1);
+    output1=output1 + output2;
+};
+
+
