ce3197425c2b11a4739574e1ba64777135a09af0
Ágnes Molnár <agnes.molnar@gmx.com>
2018-10-29 16:32:56 +0100

    created fizz-buzz.ts

---
+++ week-02/day-1/fizz-buzz.ts
@@ -0,0 +1,19 @@
+
+for(let j: number = 1; j<=100; j++){
+    if( (j%5 ==0) && (j%3 ==0)){
+
+        console.log("FizzBuzz");
+
+    } else if ( j%5 ==0){
+
+        console.log("Buzz");
+
+    } else if (j%3 ==0){
+
+        console.log("Fizz");
+        
+    } else {
+
+        console.log(j);
+    }
+    };
