d887e96c25b6c98c254002c58096d80483a0ea5e
Ágnes Molnár <agnes.molnar@gmx.com>
2018-11-04 19:01:29 +0100

    modified draw-pyramid.ts

--- week-02/day-1/draw-pyramid.ts
+++ week-02/day-1/draw-pyramid.ts
@@ -2,13 +2,20 @@
 
 let lineCount: number = 4;
 
-for (var i = 0; i < lineCount; i++) {
-    var str = '';
-    for (var j = 1; j < lineCount-i; j++) {
-      str = str + ' ';
-    }
-    for (var k = 1; k <= (2*i+1); k++) {
-      str = str + '*';
-    }
+
+for( let i: number = 0; i < lineCount; i++){
+       
+        let str = '';
+
+      for(let j: number = 1; j < lineCount-i; j++){
+
+        str = str + ' ';
+
+      }
+
+      for( let k = 1; k <= (2*i+1); k++ ){
+
+        str = str + '*';
+      } 
     console.log(str);
-  }
+};
