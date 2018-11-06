c4255b96c1249304ffa9a444d3ba9f29ce9c5965
Ágnes Molnár <agnes.molnar@gmx.com>
2018-11-06 12:20:11 +0100

    fixed urlfixer.ts

--- week-03/day-1/url-fixer.ts
+++ week-03/day-1/url-fixer.ts
@@ -1,10 +1,20 @@
 'use strict';
 // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
 // Also, the URL is missing a crutial component, find out what it is and insert it too!
-
 let url: string = 'https//www.reddit.com/r/nevertellmethebots';
 
-let newUrl: string = url.replace("bots","odds");
+function fixUrl(wrUrl){
 
-console.log(newUrl);
+    let fixUrl = wrUrl.replace("bots", "odds").replace("//","://");
+    return fixUrl;
+
+}
+url = fixUrl(url);
+
+console.log(url);
+
+
+
+
+
 
