61131e5178c4bc0ef4094f9745b4aeca634de2e1
Ágnes Molnár <agnes.molnar@gmx.com>
2018-11-06 21:24:26 +0100

    added comment

--- week-02/day-1/draw-pyramid.ts
+++ week-02/day-1/draw-pyramid.ts
@@ -19,3 +19,17 @@ for( let i: number = 0; i < lineCount; i++){
       } 
     console.log(str);
 };
+
+
+
+//let lineCount: number = 4;
+//let star: string = '*';
+//let space: string = ' ';
+
+//for (let i = 1; i <= lineCount; i++) {
+
+//  let spaceCount: number = lineCount - i;
+//    let starCount: number =  (i - 1) * 2 + 1;
+//    let pyramid: string = space.repeat(spaceCount) + star.repeat(starCount);
+//    console.log(pyramid);
+
