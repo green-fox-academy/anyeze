ac6f8ad7b0e42e32a242a4e0c2b5506b69aa5d00
Ágnes Molnár <agnes.molnar@gmx.com>
2018-11-07 21:53:53 +0100

    created starry-night

--- week-03/day-2/starry-night/starry-night-ts.ts
+++ week-03/day-2/starry-night/starry-night-ts.ts
@@ -11,3 +11,11 @@ const ctx = canvas.getContext('2d');
 //  - The stars should have random positions on the canvas
 //  - The stars should have random color (some shade of grey)
 
+for(let i = 0; i< 200; i++){
+    
+    let color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+    
+    ctx.fillStyle = color;
+
+    ctx.fillRect(Math.random()*canvas.width, Math.random()*canvas.height, 15, 15);
+}
