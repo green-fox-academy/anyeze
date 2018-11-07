7340fc18934a4f890a2c3f9af744381abcff27b1
Ágnes Molnár <agnes.molnar@gmx.com>
2018-11-07 20:47:11 +0100

    completed functiontocenter task

--- week-03/day-2/function-to-center/function-to-center-ts.ts
+++ week-03/day-2/function-to-center/function-to-center-ts.ts
@@ -9,3 +9,31 @@ const ctx = canvas.getContext('2d');
 // The x and y coordinates of the line's starting point
 // and draws a line from that point to the center of the canvas.
 // Fill the canvas with lines from the edges, every 20 px, to the center.
+
+function drawLine(x,y){
+
+ctx.beginPath();
+ctx.moveTo(canvas.width/2, canvas.height/2);
+ctx.lineTo(x, y);
+ctx.stroke();
+}
+
+
+for(let i = 0; i < canvas.width; i+=20){
+            drawLine(i,0)
+        }
+
+for(let j = 0; j < canvas.height; j+=20){
+          
+            drawLine(canvas.width, j)
+        }
+
+for(let k = 0; k <= canvas.height; k+=20){
+          
+        drawLine(0, k)
+}
+
+for(let d = 0; d <= canvas.width; d+=20){
+
+    drawLine(d,canvas.height)
+} 
