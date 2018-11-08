97a2c9dc5c35c7359ad7e3ecba3ac2dc9c7d523c
Ágnes Molnár <agnes.molnar@gmx.com>
2018-11-08 12:30:34 +0100

    added alternative solution

--- week-03/day-1/candyshop.ts
+++ week-03/day-1/candyshop.ts
@@ -1,7 +1,7 @@
 'use strict';
 export {};
 
-let shopItems: any [] = ['Cupcake', 2, 'Brownie', false];
+
 
 // Accidentally we added "2" and "false" to the array.
 // Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
@@ -9,15 +9,40 @@ let shopItems: any [] = ['Cupcake', 2, 'Brownie', false];
 // Create a function called sweets() which takes the list as a parameter.
 // Expected output: "Cupcake", "Croissant", "Brownie", "Ice cream"
 
-function sweets(list: any [] ){
-       
-    list.splice(1, 1, "Croissant" );
-    list.splice(3, 1, "Ice cream");
-   return list;
-
-}
+let shopItems: any [] = ['Cupcake', 2, 'Brownie', false];
 
 
-console.log(sweets(shopItems));
+function sweets(array){
 
-export = sweets;
+        let newArray = array.map(function(element){
+
+                if(typeof element === 'number'){
+                        return "Croissant";
+                }else if(typeof element === "boolean"){
+
+                        return 'Ice cream';
+                }
+
+                return element;})
+
+
+         return newArray;}
+
+
+                console.log(sweets(shopItems));    
+
+
+                // first solution
+                //function sweets(list: any [] ){
+
+                //        list.splice(1, 1, "Croissant" );
+                //        list.splice(3, 1, "Ice cream");
+                      
+                //        return list;
+                    
+                //    }
+                    
+                    
+                //  console.log(sweets(shopItems));
+                    
+                  export = sweets;
