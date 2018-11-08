9fb4bb28d1d1afc63acc7b188d6ba5c602d1fb3c
Ágnes Molnár <agnes.molnar@gmx.com>
2018-11-08 09:58:01 +0100

    set the colors to random grey shaedes

--- week-03/day-2/starry-night/starry-night-ts.ts
+++ week-03/day-2/starry-night/starry-night-ts.ts
@@ -11,11 +11,15 @@ const ctx = canvas.getContext('2d');
 //  - The stars should have random positions on the canvas
 //  - The stars should have random color (some shade of grey)
 
-for(let i = 0; i< 200; i++){
-    
-    let color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
-    
-    ctx.fillStyle = color;
 
-    ctx.fillRect(Math.random()*canvas.width, Math.random()*canvas.height, 15, 15);
+
+for(let i = 0; i< 500; i++){
+    
+      
+    ctx.fillStyle = "hsl(" + 360 * Math.random() + ',' +
+    (0 * Math.random()) + '%,' + 
+    (80 * Math.random()) + '%)';
+
+
+    ctx.fillRect(Math.random()*canvas.width, Math.random()*canvas.height, 3, 3);
 }
