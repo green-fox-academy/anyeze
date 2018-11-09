08ef208510f5b2884f173b511baebba06da1d232
Ágnes Molnár <agnes.molnar@gmx.com>
2018-11-09 09:02:47 +0100

    fixed diagonals task

--- week-03/day-2/diagonals/diagonals.ts
+++ week-03/day-2/diagonals/diagonals.ts
@@ -14,3 +14,12 @@ ctx.beginPath();
 ctx.moveTo(0, 0);
 ctx.lineTo(canvas.width, canvas.height);
 ctx.stroke();
+
+
+
+
+ctx.strokeStyle = "red";
+ctx.beginPath();
+ctx.moveTo(canvas.width, 0);
+ctx.lineTo( 0, canvas.height);
+ctx.stroke();
