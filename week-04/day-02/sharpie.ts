2bdb612b33dfa57b66f7920954e2bb019aae1b0c
Ágnes Molnár <agnes.molnar@gmx.com>
2018-11-13 14:20:06 +0100

    completed pokemon task

--- week-04/day-02/sharpie.ts
+++ week-04/day-02/sharpie.ts
@@ -10,12 +10,13 @@ constructor (color: string, width: number){
 }
 
 public use(){
-    this.inkAmount-=10;
+    this.inkAmount-=1;
 }
 }
 
 let lila = new Sharpie("lila", 52.58888 );
-
+let blue = new Sharpie("blue",89)
 lila.use();
 
 console.log(lila);
+console.log(blue)
