8de0a139ea28d9c8cf8a3f0f28d41e45a0144a2a
Ágnes Molnár <agnes.molnar@gmx.com>
2018-11-15 15:14:12 +0100

    completed string2 task

---
+++ week-04/day-03/string2.ts
@@ -0,0 +1,18 @@
+// Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
+function change2(str: string){
+
+    if( str.indexOf('x') === -1){
+       
+        return str;
+
+    }else{
+
+        return change2(str.replace('x', ' '));
+    
+    }
+
+}
+let exampleString = " xoxoxoxoxoxoxo yoyoyoyoyoyoyoyoyoyo "
+
+console.log(change2(exampleString));
