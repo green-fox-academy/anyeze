4826194c93a111198838589597ef8c3c375754d5
Ágnes Molnár <agnes.molnar@gmx.com>
2018-11-17 13:44:41 +0100

    completed reversed-lines.ts

---
+++ week-04/day-01/reversed-lines.ts
@@ -0,0 +1,25 @@
+export { };
+const fs = require('fs');
+
+function readFromFile(fileName: string): string {
+  try {
+    return fs.readFileSync(fileName, 'utf-8');
+  } catch (e) {
+    console.log(e.message);
+    return null;
+  }
+}
+function reversedLines(fileName: string){
+    let fileContent: string[] = readFromFile(fileName).split('\r\n');
+    let reverse: string[]=[];
+
+    fileContent.forEach(function(e){
+
+        reverse.push(e.split('').reverse().join(''));
+
+        
+    });
+    return reverse;
+}
+
+console.log(reversedLines("reversed-lines.txt"));
