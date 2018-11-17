87ec66d7212fcc8c27392ea79c71912edd376aef
Ágnes Molnár <agnes.molnar@gmx.com>
2018-11-17 15:12:25 +0100

    added encoded-lines.ts

--- week-04/day-01/reversed-lines.ts
+++ week-04/day-01/reversed-lines.ts
@@ -2,24 +2,22 @@ export { };
 const fs = require('fs');
 
 function readFromFile(fileName: string): string {
-  try {
-    return fs.readFileSync(fileName, 'utf-8');
-  } catch (e) {
-    console.log(e.message);
-    return null;
-  }
+    try {
+        return fs.readFileSync(fileName, 'utf-8');
+    } catch (e) {
+        console.log(e.message);
+        return null;
+    }
 }
-function reversedLines(fileName: string){
+function reversedLines(fileName: string): string {
+
     let fileContent: string[] = readFromFile(fileName).split('\r\n');
-    let reverse: string[]=[];
+    let reverse: string[] = [];
 
-    fileContent.forEach(function(e){
-
+    fileContent.forEach(function (e) {
         reverse.push(e.split('').reverse().join(''));
-
-        
     });
-    return reverse;
+    return reverse.join('\r\n');
 }
 
 console.log(reversedLines("reversed-lines.txt"));
