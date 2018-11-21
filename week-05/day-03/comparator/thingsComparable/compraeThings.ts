dd3d4835cfa64ffa795050728801918b59d0f9ef
Ágnes Molnár <agnes.molnar@gmx.com>
2018-11-21 16:22:52 +0100

    fixed comparethings

--- week-05/day-03/comparator/thingsComparable/compraeThings.ts
+++ week-05/day-03/comparator/thingsComparable/compraeThings.ts
@@ -1,5 +1,43 @@
 interface Comparable {
-    compareTo(other: Comparable): number;
-    
+  compareTo(other: Comparable): number;
+
+}
+
+class Thing implements Comparable {
+  private name: string;
+  private completed: boolean;
+
+  constructor(name: string, completed: boolean) {
+    this.name = name;
+    this.completed = completed;
   }
 
+  public getName(): string {
+    return this.name;
+  }
+  compareTo(other: Thing): number {
+    if (this.completed === other.completed) {
+      return 0;
+    } else if (this.completed > other.completed) {
+      return -1;
+    } else {
+      return 1;
+    }
+  };
+}
+
+let listOfThings: Thing[] = initializeThing();
+
+function initializeThing(): Thing[] {
+  return [
+    new Thing('Get milk', true),
+    new Thing('Remove the obstacles', false),
+    new Thing('Stand up', true),
+    new Thing('Eat lunch', false)
+  ];
+}
+listOfThings.sort(function (a: Thing, b: Thing): number {
+  return a.compareTo(b);
+});
+console.log(listOfThings);
+export { Thing };
