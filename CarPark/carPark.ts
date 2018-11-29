77ebe2ca37799b71f835257066bd33432d459e55
Ágnes Molnár <agnes.molnar@gmx.com>
2018-11-29 16:10:08 +0100

    added carpark

---
+++ CarPark/carPark.ts
@@ -0,0 +1,80 @@
+'use strict';
+import { Car } from './car';
+import { readFromFile, writeToFile, appendToFile } from './file-io';
+
+class CarPark {
+  private myCars: Car[] = [];
+  addCar(car: Car): void {
+    this.myCars.push(car);
+  }
+
+  getMyCars(): Car[] {
+    return this.myCars;
+  }
+
+  addCarToFile(car: Car) {
+    appendToFile('cars.csv', `${car.getLicensePlate()},${car.getManucaturerYear()},${car.getTicker()}\r\n`);
+  }
+
+  removeCar(car: Car) {
+    this.myCars.splice(this.myCars.indexOf(car), 1);
+  }
+
+  removeById(id: number) {
+    this.myCars.forEach((e, i) => {
+      if (e.getId() === id) {
+        this.myCars.splice(i, 1);
+      }
+    });
+    return this.myCars;
+  }
+
+  getOldest(fileName: string): string {
+    let content = readFromFile(fileName);
+    let array = [];
+    if (content !== null) {
+      content.split('\r\n').forEach(e => {
+        array.push(e.split(','));
+      });
+
+      let index = 0;
+      let smallest = 9999;
+      array.forEach((e, i) => {
+        console.log(e);
+        if (e[1] < smallest) {
+          index = i;
+          smallest = e[1];
+        }
+      });
+      return array[index][0];
+    }
+  }
+
+  getPenalty(fileName: string) {
+    let content = readFromFile(fileName);
+    let noParkingTicket: string[] = [];
+    if (content !== null) {
+      content.split('\r\n').forEach(e => {
+        if(e.split(',')[2] === '0') {
+          noParkingTicket.push(e.split(',')[0]);
+        }
+      });
+    }
+    return noParkingTicket;
+  }
+}
+
+let myCar = new Car(12, 'ABCD-92', 1984, 1);
+let myCar1 = new Car(14, 'ABCD-92', 1984, 1);
+let myCar2 = new Car(2, 'ABCD-92', 1984, 1);
+let myCarPark = new CarPark();
+myCarPark.addCar(myCar);
+myCarPark.addCar(myCar1);
+myCarPark.addCar(myCar2);
+// console.log(myCarPark.removeCar(myCar));
+// console.log(myCarPark.removeById(2));
+// console.log(myCarPark.getMyCars());
+// myCarPark.addCarToFile(myCar);
+
+// console.log(myCarPark.getOldest('cars.csv'));
+console.log(myCarPark.getPenalty('cars.csv'));
