7b02f24a1db756f38767eacfb5e10950b05b3e97
Ágnes Molnár <agnes.molnar@gmx.com>
2018-12-11 15:11:26 +0100

    feat(giphy): finished task

---
+++ week-07/day-02/giphy-api/main.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const myApiKey = 'wS1TLtmLbCysrjk0x1DTXLQtZGACcDcb';
+const url = `https://api.giphy.com/v1/gifs/search?api_key=${myApiKey}&q=funny+cat&limit=16&offset=0&rating=G&lang=en`;
+
+let xhr = new XMLHttpRequest();
+xhr.open('GET', url, true);
+
+xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            console.log(response.data);
+            response.data.forEach(e => {
+                let body = document.querySelector('body');
+                let images = document.createElement('img');
+                body.appendChild(images);
+                images.src = e.images.original_still.url;
+                images.onclick = () => {
+                images.src = e.images.preview_gif.url;
+                }
+            });
+        }
+
+    }
+}
+xhr.send();
+
