5ec44287b341475c46aba755db67205d86e81cf1
Ágnes Molnár <agnes.molnar@gmx.com>
2018-12-12 18:45:46 +0100

    started candygame

---
+++ week-07/day-03/candygame/game.js
@@ -0,0 +1,9 @@
+let createCandy = document.querySelector('./create-candies');
+let buyLollipops = document.querySelector('./buy-lollipops');
+let candyMachine = document.querySelector('./candy-machine');
+
+
+
+createCandy.addEventListener('click', event =>{
+    
+})
