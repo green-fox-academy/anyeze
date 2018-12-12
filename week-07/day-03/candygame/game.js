4adddbf41046c1a4f8bc7ec150b513ae3c8d4782
Ágnes Molnár <agnes.molnar@gmx.com>
2018-12-12 21:39:49 +0100

    finished task

--- week-07/day-03/candygame/game.js
+++ week-07/day-03/candygame/game.js
@@ -1,9 +1,40 @@
-let createCandy = document.querySelector('./create-candies');
-let buyLollipops = document.querySelector('./buy-lollipops');
-let candyMachine = document.querySelector('./candy-machine');
+'use strict'
+let createCandy = document.querySelector('.create-candies');
+let buyLollypops = document.querySelector('.buy-lollypops');
+let candyMachine = document.querySelector('.candy-machine');
 
+let candyCounter = document.querySelector('.candies');
+let candySecond = document.querySelector('.speed');
+let lollypops = document.querySelector('.lollypops');
 
+let candyRainRate = 1;
+let counter = 1;
 
-createCandy.addEventListener('click', event =>{
-    
+createCandy.addEventListener('click', (e) => {
+  counter++;
+  candyCounter.innerHTML = counter;
 })
+
+let lollyPopCounter = 3;
+buyLollypops.addEventListener('click', (e) => {
+  if (counter >= 100) {
+    counter -= 100;
+    lollypops.innerHTML += '🍭';
+    lollyPopCounter++;
+    candyCounter.innerHTML = counter;
+  }
+})
+
+const makeCandyRain = () => {
+  let candyProductionRate = Math.floor(lollyPopCounter / 10) * candyRainRate;
+  counter += candyProductionRate;
+  candyCounter.innerHTML = counter;
+  candySecond.innerText = candyProductionRate;
+}
+
+candyMachine.addEventListener('click', (e) => {
+  candyRainRate *= 10;
+
+})
+
+setInterval(makeCandyRain,1000);
