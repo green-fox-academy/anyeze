e820fc51c6b530dffb38381dc00403093c3d3248
Ágnes Molnár <agnes.molnar@gmx.com>
2018-12-17 15:52:35 +0100

    finished task

---
+++ week-08/todos/main.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const todos = [
+    'get up',
+    'survive',
+    'go back to bed',
+];
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+
+    const todos = [
+        'get up',
+        'survive',
+        'go back to bed',
+    ];
+
+    res.render('home', {
+        todos: todos
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
