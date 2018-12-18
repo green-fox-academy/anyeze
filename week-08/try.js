95f6cd0b18b4c0e925c58d43aaf964be07bef8ee
Ágnes Molnár <agnes.molnar@gmx.com>
2018-12-18 14:39:47 +0100

    try new settings

---
+++ week-08/try.js
@@ -0,0 +1,30 @@
+
+xhr.onreadystatechange = () => {
+	if (xhr.readyState === XMLHttpRequest.DONE) {
+		if (xhr.status === 200) {
+			const response = JSON.parse(xhr.responseText);
+			console.log(response.response.docs);
+			response.response.docs.forEach(e => {
+				const body = document.querySelector('body');
+				const article = document.createElement('article');
+				const headline = document.createElement('h1');
+				const snippet = document.createElement('p');
+				const publicationDate = document.createElement('p1');
+				const permaLink = document.createElement('a');
+
+				headline.innerText = e.headline.main;
+				snippet.innerText = e.snippet;
+				permaLink.href = e.web_url;
+				permaLink.innerText = '\r\n Go to page';
+				publicationDate.innerText = e.pub_date;
+
+				  body.appendChild(article);
+				article.appendChild(headline);
+				article.appendChild(snippet);
+				article.appendChild(publicationDate);
+				article.appendChild(permaLink);
+			});
+		}
+	}
+}
+xhr.send();
