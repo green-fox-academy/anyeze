6f17d9a6aadd9dbcb717be774c57d7f54df24dc7
Ágnes Molnár <agnes.molnar@gmx.com>
2018-12-18 14:30:11 +0100

    fixed styleguide

--- week-07/day-02/new-york-times/main.js
+++ week-07/day-02/new-york-times/main.js
@@ -1,37 +1,36 @@
 'use strict'
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=14615f9ac6e54bf2b264b431b36c1448&q=the+moon+landing+by+Apollo+11";
+const url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=14615f9ac6e54bf2b264b431b36c1448&q=the+moon+landing+by+Apollo+11";
 
-let xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
 xhr.open('GET', url, true);
 
 xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-            const response = JSON.parse(xhr.responseText);
-            console.log(response.response.docs);
-            response.response.docs.forEach(e => {
-                let body = document.querySelector('body');
-                let article = document.createElement('article');
-                let headline = document.createElement('h1');
-                let snippet = document.createElement('p');
-                let publicationDate = document.createElement('p1');
-                let permaLink = document.createElement('a');
+	if (xhr.readyState === XMLHttpRequest.DONE) {
+		if (xhr.status === 200) {
+			const response = JSON.parse(xhr.responseText);
+			console.log(response.response.docs);
+			response.response.docs.forEach(e => {
+				const body = document.querySelector('body');
+				const article = document.createElement('article');
+				const headline = document.createElement('h1');
+				const snippet = document.createElement('p');
+				const publicationDate = document.createElement('p1');
+				const permaLink = document.createElement('a');
 
-                body.appendChild(article);
-                article.appendChild(headline);
-                article.appendChild(snippet);
-                article.appendChild(publicationDate);
-                article.appendChild(permaLink);
+				headline.innerText = e.headline.main;
+				snippet.innerText = e.snippet;
+				permaLink.href = e.web_url;
+				permaLink.innerText = '\r\n Go to page';
+				publicationDate.innerText = e.pub_date;
 
-                headline.innerText = e.headline.main;
-                snippet.innerText = e.snippet;
-                permaLink.href = e.web_url;
-                permaLink.innerText = "\r\n Go to page";
-                publicationDate.innerText = e.pub_date;
-
-            });
-        }
-    }
+				body.appendChild(article);
+				article.appendChild(headline);
+				article.appendChild(snippet);
+				article.appendChild(publicationDate);
+				article.appendChild(permaLink);
+			});
+		}
+	}
 }
 xhr.send();
