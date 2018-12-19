fff42cb6cf492597fd613af313f3b32b888455a6
Ágnes Molnár <agnes.molnar@gmx.com>
2018-12-19 17:18:22 +0100

    finished task

--- week-08/day-03/exercise1/main.js
+++ week-08/day-03/exercise1/main.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = 8080;
 
 app.use('/assets', express.static('assets'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-
-});
 
 app.get('/doubling', (req, res) => {
   if (req.query.input) {
